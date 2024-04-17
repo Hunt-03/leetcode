37c19f83309d2af666fb6c44a976c97501ab9cb7
Hunt-03 <131003334+Hunt-03@users.noreply.github.com>
2024-04-17 22:28:49 +0530

    Time: 2 ms (99.33%), Space: 44.9 MB (58.17%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1 || numRows >= s.length()) {
+            return s;
+        }
+
+        StringBuilder result = new StringBuilder();
+        int cycleLen = 2 * numRows - 2;
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j + i < s.length(); j += cycleLen) {
+                result.append(s.charAt(j + i));
+                if (i != 0 && i != numRows - 1 && j + cycleLen - i < s.length()) {
+                    result.append(s.charAt(j + cycleLen - i));
+                }
+            }
+        }
+
+        return result.toString();
+    }
+}
