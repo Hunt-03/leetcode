36196756efabad846b5056c2f940bb04accfb0e5
Hunt-03 <131003334+Hunt-03@users.noreply.github.com>
2024-05-15 21:27:50 +0530

    Time: 1 ms (100%), Space: 44.1 MB (85.94%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        int[][] r=new int[9][9];
+        int[][] c=new int[9][9];
+        int[][] b=new int[9][9];
+        for (int i=0;i<9;i++){
+            for (int j=0;j<9;j++) {
+                if (board[i][j]=='.')
+                continue;
+                int num=board[i][j]-'1';
+                int bo=(i/3)*3+j/3;
+                if (r[i][num]==1)
+                return false;
+                if (c[j][num]==1)
+                return false;
+                if (b[bo][num]==1)
+                return false;
+                r[i][num]=1;
+                c[j][num]=1;
+                b[bo][num]=1;
+            }
+        }
+        return true;
+    }
+}
