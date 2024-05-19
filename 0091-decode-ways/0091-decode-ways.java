cf893bff641b7c174dd0d806cd1116adf8dec1d6
Hunt-03 <131003334+Hunt-03@users.noreply.github.com>
2024-05-19 20:53:42 +0530

    Time: 1 ms (80.37%), Space: 41.3 MB (92.96%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numDecodings(String s) {
+        if (s.charAt(0) == '0') {
+            return 0;
+        }
+
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int oneDigit = s.charAt(i - 1) - '0';
+            int twoDigits = (s.charAt(i - 2) - '0') * 10 + oneDigit;
+
+            if (oneDigit >= 1) {
+                dp[i] += dp[i - 1];
+            }
+            if (twoDigits >= 10 && twoDigits <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[n];
+    }
+}
