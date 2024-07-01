261a3b1bd694bc673dcc301a1cd5dcabbf5cfb58
Hunt-03 <131003334+Hunt-03@users.noreply.github.com>
2024-07-01 14:20:27 +0530

    Time: 6 ms (9.09%), Space: 51.8 MB (99.98%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -15,7 +15,30 @@ class Solution {
                 right--;
             }
         }
-
+System.gc();
         return maxArea;
+
+        
     }
 }
+// import java.util.*;
+// class Solution {
+//     public int maxArea(int[] height) {
+//         int n = height.length;
+//         int start =0;
+//         int end = height.length-1;
+//         int maxarea = Integer.MIN_VALUE;
+//         while (start < end) {
+//             if(height[start]<height[end]){
+//                 maxarea = Math.max(maxarea,height[start]*(end-start));
+//                 start ++;
+//             }
+//             else {
+//                 maxarea = Math.max(maxarea,height[end]*(end-start));
+//                 end--;
+
+//             }
+//     }
+//     return maxarea;
+// }
+// }
